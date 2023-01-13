db8b9122e36020e04905140f5e0278c0121d55c8
GalletaOreo98 <christophergironcabrera@gmail.com>
2023-01-13 17:44:51 -0600

    Add - Home y acc-list pages

--- src/app/accounting/accounting.module.ts
+++ src/app/accounting/accounting.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { AccountingRoutingModule } from './accounting-routing.module';
+import { AccListComponent } from './pages/acc-list/acc-list.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    AccListComponent
+  ],
   imports: [
     CommonModule,
     AccountingRoutingModule
---
+++ src/app/accounting/pages/acc-list/acc-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-acc-list',
+  templateUrl: './acc-list.component.html',
+  styleUrls: ['./acc-list.component.css']
+})
+export class AccListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
